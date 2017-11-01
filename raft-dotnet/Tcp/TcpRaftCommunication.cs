8b7e4eb39a92c050392ef7164c775576a7bf6792
Justin Pealing <justin.pealing@audatex.com>
2017-11-01 10:04:12 +0000

    Combine TcpRaftServer into communication

--- raft-dotnet/Tcp/TcpRaftCommunication.cs
+++ raft-dotnet/Tcp/TcpRaftCommunication.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using ProtoBuf;
 using raft_dotnet.Communication;
 
 namespace raft_dotnet.Tcp
@@ -6,6 +10,7 @@ namespace raft_dotnet.Tcp
     public class TcpRaftCommunication : IRaftCommunication, IDisposable
     {
         private string _listenAddress;
+        private TcpListener _listener;
 
         public TcpRaftCommunication(string listenAddress)
         {
@@ -36,11 +41,58 @@ namespace raft_dotnet.Tcp
 
         public void Start()
         {
+            _listener = new TcpListener(IPAddress.Loopback, 13000);
+            _listener.Start();
+            BeginAcceptClient(_listener);
+        }
 
+        private void BeginAcceptClient(TcpListener listener)
+        {
+            listener.BeginAcceptTcpClient(ar =>
+            {
+                var client = listener.EndAcceptTcpClient(ar);
+                ClientLoop(client);
+                BeginAcceptClient(listener);
+            }, null);
+        }
+
+        private async Task ClientLoop(TcpClient client)
+        {
+            try
+            {
+                await ClientLoopInner(client);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.WriteLine("Client disconnected");
+        }
+
+        private async Task ClientLoopInner(TcpClient client)
+        {
+            Console.WriteLine("Client connected");
+            using (var stream = client.GetStream())
+            {
+                while (client.Connected)
+                {
+                    var request = Serializer.Deserialize<Request>(stream);
+                    OnMessage(new RaftMessageEventArgs
+                    {
+                        Message = (RaftMessage) request.Arguments
+                    });
+                }
+            }
         }
 
         public void Dispose()
         {
+            _listener.Server.Dispose();
+        }
+
+        protected virtual void OnMessage(RaftMessageEventArgs e)
+        {
+            Message?.Invoke(this, e);
         }
     }
 }
